3fb216c2017e2bcd8d770f0b00fe6a2431e9eac1
corentinlabbe <labbe@et.esiea.fr>
2019-06-05 13:35:52 +0200

    Etape première, l'appli se ferme automatiquement dès son ouverture

--- app/src/main/java/com/example/alarmwme/MainActivity.java
+++ app/src/main/java/com/example/alarmwme/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.alarmwme;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public abstract class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+   // abstract setContentView(R.layout.activity_main);
 }
+
+
